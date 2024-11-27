55683b07cd64a1fc4c6a8461c778c22121e1b3e5
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 17:03:10 +0100

    refactor: simplify client configuration and clean up function formatting in sanity_helpers.js

--- migrations/reuters/sanity_helpers.js
+++ migrations/reuters/sanity_helpers.js
@@ -1,15 +1,14 @@
-import { createClient } from '@sanity/client';
+import {createClient} from '@sanity/client'
 import {configDotenv} from 'dotenv'
 configDotenv()
 
-
 const client = createClient({
-    "projectId": "v0gkry1w",
-    "dataset": "production",
-    "useCdn": true,
-    "apiVersion": new Date().toISOString().split('T')[0],
-    "token": process.env.token
-});
+  projectId: 'v0gkry1w',
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: new Date().toISOString().split('T')[0],
+  token: process.env.token,
+})
 
 // const client2 = createClient({
 //     "projectId": "v0gkry1w",
@@ -19,36 +18,32 @@ const client = createClient({
 //     "token": process.env.token
 // });
 
-export async function createReuterPost(post)
-{
-    const result = await client.create(post);
-    return result._id;
+export async function createReuterPost(post) {
+  const result = await client.create(post)
+  return result._id
 }
 
-export async function createArticle(article)
-{
-    const result = await client.create(article);
-    return result._id;
+export async function createArticle(article) {
+  const result = await client.create(article)
+  return result._id
 }
 
-export async function getAuthor(author)
-{
-    const query = `
+export async function getAuthor(author) {
+  const query = `
         *[
         _type == "author" && name == $author
         ]{name, _id}
-    `;
-    const result = await client.fetch(query, {author});
-    return result;
+    `
+  const result = await client.fetch(query, {author})
+  return result
 }
 
-export async function getCategory(list)
-{
-    const query = `
+export async function getCategory(list) {
+  const query = `
         *[
             _type == "category" && name in $list
         ]{name, _id}
-    `;
-    const result = await client.fetch(query, {list});
-    return result;
+    `
+  const result = await client.fetch(query, {list})
+  return result
 }
