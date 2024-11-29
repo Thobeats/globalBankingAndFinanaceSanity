eacce0d0e46aac768dffefb0aca77264747772e6
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 07:56:26 +0100

    update the sanity client import statement

--- migrations/reuters/sanity_helpers.js
+++ migrations/reuters/sanity_helpers.js
@@ -1,4 +1,4 @@
-import {createClient} from '@sanity/client'
+import createClient from '@sanity/client'
 import {configDotenv} from 'dotenv'
 import { Readable } from 'stream';
 import {uuid} from '@sanity/uuid'
